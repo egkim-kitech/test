903902945db8cca199b7dc2dcaf64e7234a08913
egkim <egkim@kitech.re.kr>
2024-12-12 11:13:45 +0900

    added dag : dags_python_import.py

---
+++ dags/dags_python_import.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+import datetime
+import pendulum
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from common.common_func import get_sftp
+import random
+
+with DAG(
+    dag_id="dags_python_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["Kitech", "airflow_test"]
+) as dag:
+    
+    task_get_sftp = PythonOperator(
+        task_id="task_get_sftp",
+        python_callable=get_sftp,
+    )
+
+    task_get_sftp
