a69b3cbb054fa99768dafb61530f08dd510de482
egkim <egkim@kitech.re.kr>
2024-12-12 10:36:28 +0900

    modified select_fruit.sh

--- plugins/select_fruit.py
+++ plugins/select_fruit.py
@@ -1,1 +1,12 @@
-aaaa
+#!/bin/bash
+FRUIT=$1
+if [ $FRUIT = "APPLE" ]; then
+    echo "You selected apple!"
+elif [ $FRUIT = "ORANGE" ]; then
+    echo "You selected orange!"
+elif [ $FRUIT = "GRAPE" ]; then
+    echo "You selected grape!"
+else 
+    echo "You selected other fruit!"
+fi
+
