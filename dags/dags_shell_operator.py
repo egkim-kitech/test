32abd00e57e42ca40e345146e17f6cd6058c43d6
egkim <egkim@kitech.re.kr>
2024-12-12 10:56:38 +0900

    modified plugins : py ->sh

--- dags/dags_shell_operator.py
+++ dags/dags_shell_operator.py
@@ -13,9 +13,11 @@ with DAG(
     dagrun_timeout=datetime.timedelta(minutes=60),
     tags=["Kitech", "airflow_test"]
 ) as dag:
-    t1 = BashOperator(
-        task_id="t1",
+    t1 = BashOperator(task_id="t1",bash_command="pwd")
+    
+    t2 = BashOperator(
+        task_id="t2",
         bash_command="/opt/airflow/plugins/select_fruit.sh APPLE",
     )
 
-    t1
+    t1 >> t2
